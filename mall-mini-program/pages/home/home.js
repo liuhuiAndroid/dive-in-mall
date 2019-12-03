832bf824efb7979e5ac57863f65018cb1a67b6b6
liu hui <im.lh@hotmail.com>
2019-12-03 14:43:25 +0800

    Initial commit

--- mall-mini-program/pages/home/home.js
+++ mall-mini-program/pages/home/home.js
@@ -37,6 +37,7 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
+    // eslint-disable-next-line no-unused-vars
     async onLoad(options) {
         this.initAllData()
         this.initBottomSpuList()
