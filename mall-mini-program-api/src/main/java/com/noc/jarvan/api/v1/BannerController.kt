8d33b84abd7b19dae47674438142f3e3114c74a8
liu hui <im.lh@hotmail.com>
2020-01-06 23:53:26 +0800

    Initial commit

--- mall-mini-program-api/src/main/java/com/noc/jarvan/api/v1/BannerController.kt
+++ mall-mini-program-api/src/main/java/com/noc/jarvan/api/v1/BannerController.kt
@@ -1,14 +1,16 @@
 package com.noc.jarvan.api.v1
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/v1/banner")
 class BannerController {
 
-    @RequestMapping("/")
+    @GetMapping("/test")
     fun test(): String {
-        return "Hello Jarvan"
+        return "Hello Jarvan2"
     }
 
 }
